1777c74ccc082b58620f12c24c2965c36366db79
Harris Nguyen <hainx1610@gmail.com>
2023-10-27 14:36:24 +0700

    chore: delete irrelevant funcs

--- controllers/notification.controllers.js
+++ controllers/notification.controllers.js
@@ -16,6 +16,7 @@ notificationController.getNotificationsByUser = catchAsync(
 
     const notifications = await Notification.find({
       forUser: userId,
+      isRead: false,
     }).sort({ createdAt: -1 });
 
     if (!notifications)
@@ -35,36 +36,5 @@ notificationController.getNotificationsByUser = catchAsync(
     );
   }
 );
-
-notificationController.getSubscription = catchAsync(async (req, res, next) => {
-  const currentUserId = req.userId;
-  const currentUserRole = req.userRole;
-
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  });
-
-  let counter = 0;
-
-  // Send a message on connection
-  res.write("event: connected\n");
-  res.write(`data: You are now subscribed!\n`);
-  res.write(`id: ${counter}\n\n`);
-  counter += 1;
-
-  // Send a subsequent message every five seconds
-  setInterval(() => {
-    // Serverside implementation of event 'current-date'
-    res.write("event: current-date\n");
-    res.write(`data: ${new Date().toLocaleString()}\n`);
-    res.write(`id: ${counter}\n\n`);
-    counter += 1;
-  }, 5000);
-
-  // Close the connection when the client disconnects
-  req.on("close", () => res.end("OK"));
-});
 
 module.exports = notificationController;
--- routes/notification.api.js
+++ routes/notification.api.js
@@ -8,7 +8,7 @@ const notificationController = require("../controllers/notification.controllers"
 const router = express.Router();
 
 /**
- * @route GET api/notifications//user/:userId
+ * @route GET api/notifications/user/:userId
  * @description Get a list of notifications for a user
  * @access private
  */
@@ -20,16 +20,6 @@ router.get(
     param("userId").exists().isString().custom(validators.checkObjectId),
   ]),
   notificationController.getNotificationsByUser
-);
-
-router.get(
-  "/subscribe",
-  authentication.accessRequired,
-  // authentication.managerRequired,
-  validators.validate([
-    // param("taskId").exists().isString().custom(validators.checkObjectId),
-  ]),
-  notificationController.getSubscription
 );
 
 module.exports = router;
