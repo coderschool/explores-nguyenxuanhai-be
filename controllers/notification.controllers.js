3b7ee6d3575a736209c4e919c97013aeeca4ea5b
Harris Nguyen <hainx1610@gmail.com>
2023-10-27 23:00:19 +0700

    chore: remove irrelevant funcs

--- controllers/notification.controllers.js
+++ controllers/notification.controllers.js
@@ -37,41 +37,6 @@ notificationController.getNotificationsByUser = catchAsync(
   }
 );
 
-notificationController.getSubscription = catchAsync(async (req, res, next) => {
-  const currentUserId = req.userId;
-  const currentUserRole = req.userRole;
-
-  res.writeHead(200, {
-    "Content-Type": "text/event-stream",
-    Connection: "keep-alive",
-    "Cache-Control": "no-cache",
-  });
-
-  let counter = 0;
-
-  // Send a message on connection
-  res.write("event: connected\n");
-  res.write(`data: You are now subscribed!\n`);
-  res.write(`id: ${counter}\n\n`);
-  counter += 1;
-
-  // Send a subsequent message every five seconds
-  let interValId = setInterval(() => {
-    // Serverside implementation of event 'current-date'
-    res.write("event: current-date\n");
-    res.write(`data: ${new Date().toLocaleString()}\n`);
-    res.write(`id: ${counter}\n\n`);
-    counter += 1;
-  }, 5000);
-
-  // Close the connection when the client disconnects
-  req.on("close", () => {
-    console.log("client dropped me");
-    clearInterval(interValId);
-    res.end("OK");
-  });
-});
-
 notificationController.getNotificationsByUserRealTime = catchAsync(
   async (req, res, next) => {
     const currentUserId = req.userId;
